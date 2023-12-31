9ed966413d49cf2196cb9ff2967916145ce4861f
Mustafa2 <Mustafauyar91@gmail.com>
2023-12-31 17:58:42 +0300

    ders5_2 tamamlanddi

--- app/src/main/java/com/mustafauyar/farkliaktivitelerprojesi/MainActivity.kt
+++ app/src/main/java/com/mustafauyar/farkliaktivitelerprojesi/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.mustafauyar.farkliaktivitelerprojesi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun aktiviteDegistir(view:View){
+        val intent = Intent(applicationContext,IkinciAktivite::class.java)
+        startActivity(intent)
     }
 }
