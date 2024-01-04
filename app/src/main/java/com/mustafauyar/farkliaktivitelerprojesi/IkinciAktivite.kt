91f8c26cfdba762af82b08e276125e652e670305
Mustafa2 <Mustafauyar91@gmail.com>
2024-01-04 17:19:32 +0300

    pencereler arassi gecis ekledim

--- app/src/main/java/com/mustafauyar/farkliaktivitelerprojesi/IkinciAktivite.kt
+++ app/src/main/java/com/mustafauyar/farkliaktivitelerprojesi/IkinciAktivite.kt
@@ -1,11 +1,17 @@
 package com.mustafauyar.farkliaktivitelerprojesi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class IkinciAktivite : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ikinci_aktivite)
+    }
+    fun aktiviteDegistir(view: View){
+        val intent = Intent(applicationContext,MainActivity::class.java)
+        startActivity(intent)
     }
 }
